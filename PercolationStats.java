700e4d9e03368bf2bad5835503bc67f2f82862ae
Yingjie <1275963@gmail.com>
2016-02-02 16:52:51 +0800

    Percolation statistics

---
+++ PercolationStats.java
@@ -0,0 +1,26 @@
+
+public class PercolationStats {
+   public PercolationStats(int N, int T){     // perform T independent experiments on an N-by-N grid
+	   
+   }
+   public double mean(){                     // sample mean of percolation threshold
+	   return 0;
+   }
+   public double stddev()                    // sample standard deviation of percolation threshold
+   {
+	   return 0;
+   }
+   public double confidenceLo(){
+	   // low  endpoint of 95% confidence interval
+	   return 0;
+   }
+   
+   public double confidenceHi(){
+	   // high endpoint of 95% confidence interval
+	   return 0;
+   }
+
+   public static void main(String[] args){
+	   // test client (described below)
+   }
+}
