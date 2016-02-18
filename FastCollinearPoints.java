27b8b21934cce23a279c1fb090edaae911b38947
Yingjie <1275963@gmail.com>
2016-02-18 21:14:37 +0800

    Fixed depulicate, replacing with ArrayList

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
@@ -12,7 +13,9 @@ public class FastCollinearPoints {
             throw new NullPointerException();
         
         int len = points.length;
-        LineSegment[] temp = new LineSegment[10000];
+        
+//        LineSegment[] temp = new LineSegment[10000];
+        ArrayList<LineSegment> temp = new ArrayList<LineSegment>();
         Arrays.sort(points);
         
 //        for (Point p : points) {
@@ -28,6 +31,8 @@ public class FastCollinearPoints {
         int index = 0;        
         double current_slope;
         int inc;
+        Point[] ext;
+        
         Point[] temp_p = new Point[len];
         for(int i = 0; i < len; i++)
             temp_p[i] = points[i];
@@ -38,12 +43,12 @@ public class FastCollinearPoints {
             
             current_slope = temp_p[i].slopeTo(points[0]);
             inc = 0;            
-            StdOut.println();
+//            StdOut.println();
             int j = 1;
-            StdOut.printf("Point %d\n", i);
+//            StdOut.printf("Point %d\n", i);
             for(j = 1; j < len; j++) {
                 double t_slope = temp_p[i].slopeTo(points[j]);
-                StdOut.printf("With %d, slope: %f, loc: %s\n", j, t_slope, points[j].toString());
+//                StdOut.printf("With %d, slope: %f, loc: %s\n", j, t_slope, points[j].toString());
                 if(t_slope == current_slope) {
                     inc++;
                 } 
@@ -52,34 +57,38 @@ public class FastCollinearPoints {
                     if(inc >= 3) {
                         // stable sort
                         if(points[j - 1].compareTo(temp_p[i]) < 0 & points[j - inc].compareTo(temp_p[i]) < 0) {
-                            temp[index++] = new LineSegment(temp_p[i], points[j - inc]);
+                            ext = find_ext(temp_p[i], points[j - inc]);
                         } else if(points[j - 1].compareTo(temp_p[i]) > 0 & points[j - inc].compareTo(temp_p[i]) > 0) {
-                            temp[index++] = new LineSegment(temp_p[i], points[j - 1]);                   
+                            ext = find_ext(temp_p[i], points[j - 1]);                   
                         }
                         else {
-                            temp[index++] = new LineSegment(points[j - inc], points[j - 1]);
-                        }                           
+                            ext = find_ext(points[j - inc], points[j - 1]);
+                        }
+                        if(ext[0].slopeTo(temp_p[i]) == Double.NEGATIVE_INFINITY) {
+                            temp.add(new LineSegment(ext[0], ext[1]));                       
+                        }
                     }     
                     inc = 1;
                 }
             }
+            
             if(inc >= 3) {
                 // stable sort
                 if(points[j - 1].compareTo(temp_p[i]) < 0 & points[j - inc].compareTo(temp_p[i]) < 0) {
-                    temp[index++] = new LineSegment(temp_p[i], points[j - inc]);
+                    ext = find_ext(temp_p[i], points[j - inc]);
                 } else if(points[j - 1].compareTo(temp_p[i]) > 0 & points[j - inc].compareTo(temp_p[i]) > 0) {
-                    temp[index++] = new LineSegment(temp_p[i], points[j - 1]);                   
+                    ext = find_ext(temp_p[i], points[j - 1]);                   
                 }
                 else {
-                    temp[index++] = new LineSegment(points[j - inc], points[j - 1]);
-                }                           
-            }
+                    ext = find_ext(points[j - inc], points[j - 1]);
+                }
+                if(ext[0].slopeTo(temp_p[i]) == Double.NEGATIVE_INFINITY) {
+                    temp.add(new LineSegment(ext[0], ext[1]));                       
+                }
+            }                          
         }
         // shrink
-        this.segments = new LineSegment[index];
-        for(int i = 0; i < index; i++) {
-            this.segments[i] = temp[i];
-        }    
+        this.segments = temp.toArray(new LineSegment[0]);  
     }
     public int numberOfSegments() {
         // the number of line segments
@@ -89,6 +98,18 @@ public class FastCollinearPoints {
     public LineSegment[] segments() {
         // the line segments
         return this.segments;
+    }
+    
+    private Point[] find_ext(Point p1, Point p2) {
+        Point[] ext = new Point[2];
+        if(p1.compareTo(p2) < 0) {
+            ext[0] = p1;
+            ext[1] = p2;
+        } else{
+            ext[0] = p2;
+            ext[1] = p1;
+        }  
+        return ext;
     }
     
     /**
@@ -115,6 +136,7 @@ public class FastCollinearPoints {
 //        }
         FastCollinearPoints fs = new FastCollinearPoints(points);
         LineSegment[] test = fs.segments();
+        StdOut.println(test[0].equals(test[1]) );
         for (int i = 0; i < test.length; i++) {
             test[i].draw();
             StdDraw.show();
