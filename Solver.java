5c0edb2d15bc0ef6805d0dc82cb078c5f2c22706
Yingjie <1275963@gmail.com>
2016-02-20 17:58:13 +0800

    Solver Template

---
+++ Solver.java
@@ -0,0 +1,22 @@
+
+public class Solver {
+    public Solver(Board initial) {
+        // find a solution
+    }
+    
+//    public boolean isSolvable() {
+//        // 
+//    }
+//
+//    public int moves() {
+//        // minimum movements
+//    }
+//    
+//    public Iterable<Board> solution() {
+//        // sequence of boards in a shortest solution, null if unsolvable
+//    }
+//    
+//    public static void main(String[] args) {
+//        
+//    }
+}
