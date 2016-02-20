7cae6e63273a591061b69884a424c9bf59fa34a3
Yingjie <1275963@gmail.com>
2016-02-20 17:55:25 +0800

    Board template

--- Board.java
+++ Board.java
@@ -3,5 +3,40 @@ public class Board {
     public Board(int[][] blocks) {
         // The constructor
     }
-
+    
+//    public int dimension() {
+//        // board Dimension N
+//    }
+//    
+//    public int hamming() {
+//        // hamming distance
+//    }
+//    
+//    public int manhattan() {
+//        // manhanttan distance
+//    }
+//    
+//    public boolean isGoal() {
+//        // reached?
+//    }
+//    
+//    public Board twin() {
+//        // a board that is obtained by exchanging any pair of blocks
+//    }
+//    
+//    public boolean equals(Object y) {
+//        // check equality
+//    }
+//    
+//    public Iterable<Board> neighbors() {
+//        // return iterator: all neighbors boards
+//    }
+    
+    public String toString() {
+        // string representation
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
