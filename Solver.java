1fca23157899d910d55a4da0577c5c907763f685
Yingjie <1275963@gmail.com>
2016-02-21 15:43:11 +0800

    Unsolvable

--- Solver.java
+++ Solver.java
@@ -7,6 +7,7 @@ import edu.princeton.cs.algs4.Stack;
 public class Solver {
     
     private SearchNode solution;
+    private boolean issolvable;
     
     private static class SearchNode implements Comparable<SearchNode> {
         private Board board;
@@ -31,6 +32,10 @@ public class Solver {
         
     public Solver(Board initial) {
         // find a solution
+        if(initial == null) {
+            throw new NullPointerException();
+        }
+        
         MinPQ<SearchNode> heap = new MinPQ<SearchNode>();
         MinPQ<SearchNode> heap_twin = new MinPQ<SearchNode>();
 
@@ -40,7 +45,7 @@ public class Solver {
         SearchNode deq = heap.delMin();
         SearchNode deq_twin = heap_twin.delMin();
         
-        while(!deq.board.isGoal()) {
+        while((!deq.board.isGoal()) && (!deq_twin.board.isGoal())) {
             for(Board item : deq.board.neighbors()) {
                 if(deq.previous != null) {
                     if(item != deq.previous.board) {
@@ -50,29 +55,58 @@ public class Solver {
                     heap.insert(new SearchNode(deq.moves + 1, item, deq));
                 }
             }
-            deq = heap.delMin();        
-        }    
-        solution = deq;
+            deq = heap.delMin(); 
+//            StdOut.println("deq_moves" + deq.weight());
+            
+            for(Board item_twin : deq_twin.board.neighbors()) {
+                if(deq_twin.previous != null) {
+                    if(item_twin != deq_twin.previous.board) {
+                        heap_twin.insert(new SearchNode(deq_twin.moves + 1, item_twin, deq_twin));
+                    }
+                } else {
+                    heap_twin.insert(new SearchNode(deq_twin.moves + 1, item_twin, deq_twin));
+                }
+            }
+            deq_twin = heap_twin.delMin();            
+//            StdOut.println("deq_twin_moves" + deq_twin.weight());
+        } 
+        if(deq.board.isGoal()) {
+            issolvable = true;
+            solution = deq;            
+        } else {
+            issolvable = false;
+            solution = null;
+        }
+        
     }
     
-//    public boolean isSolvable() {
-//        // 
-//    }
-//
+    public boolean isSolvable() {
+        // 
+        return issolvable;
+    }
+
     public int moves() {
         // minimum movements
-        return solution.moves;
+        if(isSolvable()) {
+            return solution.moves;            
+        } else {
+            return -1;
+        }
     }
     
     public Iterable<Board> solution() {
         // sequence of boards in a shortest solution, null if unsolvable
-        Stack<Board> sol = new Stack<Board>();
-        SearchNode temp = solution;
-        while(temp != null) {
-            sol.push(temp.board);
-            temp = temp.previous;
-        }
-        return sol;
+        if(isSolvable()) {
+            Stack<Board> sol = new Stack<Board>();
+            SearchNode temp = solution;
+            while(temp != null) {
+                sol.push(temp.board);
+                temp = temp.previous;
+            }
+            return sol;
+        } else {
+            return null;
+        }   
     }
     
     public static void main(String[] args) {
@@ -80,14 +114,21 @@ public class Solver {
            In in = new In(args[0]);
            int N = in.readInt(); 
            int[][] blocks = new int[N][N];
+           
            for (int i = 0; i < N; i++)
                for (int j = 0; j < N; j++)
                    blocks[i][j] = in.readInt(); 
-           Board initial = new Board(blocks);
            
+           Board initial = new Board(blocks);           
            Solver solver = new Solver(initial);
-           StdOut.println("Minimum number of moves = " + solver.moves());
-           for (Board board : solver.solution()) 
-               StdOut.println(board);
+           
+           // print solution to standard output
+           if (!solver.isSolvable())
+               StdOut.println("No solution possible");
+           else {
+               StdOut.println("Minimum number of moves = " + solver.moves());
+               for (Board board : solver.solution())
+                   StdOut.println(board);
+           }
     }
 }
