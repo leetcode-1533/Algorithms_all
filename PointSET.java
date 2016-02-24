45421d431112eaf31987b3b8400886c8ec164020
Yingjie <1275963@gmail.com>
2016-02-24 21:19:22 +0800

    Ass 5

---
+++ PointSET.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+ 
+
+public class PointSET {
+    
+    
+    
+    public static void main(String[] args) {
+        StdOut.println("Hello Ass5");
+        
+    }
+
+}
