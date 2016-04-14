4f12f5fc996f44b46089e366b0cf55796af4af48
Yingjie <1275963@gmail.com>
2016-04-14 18:17:13 +0800

    feat: First flownetwork, workign on vertex ID

--- BaseballElimination.java
+++ BaseballElimination.java
@@ -3,6 +3,8 @@ import edu.princeton.cs.algs4.EdgeWeightedGraph;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.SeparateChainingHashST;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
 
 public class BaseballElimination {
     private class Team {
@@ -31,6 +33,8 @@ public class BaseballElimination {
         numteam = in.readInt();
         games = new EdgeWeightedGraph(numteam);
         t2id = new SeparateChainingHashST<String, Team>(numteam);
+        id2t = new SeparateChainingHashST<Integer, String>(numteam); // dual hash table
+        
         for(int i = 0; i < numteam; i++) { // read each line
             String teamname = in.readString();
             
@@ -47,7 +51,45 @@ public class BaseballElimination {
                 }
             }          
         }
-        in.close();
+        in.close();  
+        StdOut.println(constrflow("Detroit"));
+    }
+    
+    private FlowNetwork constrflow(String team) {
+        /*
+         * Documentation:
+         * Source ID: 0
+         * sink ID: 1
+         * competitions: from 2 -> game.E() - game.degree(id) + 1: length: game.E() - game.degree(id)
+         * team: from game.E() - game.degree(id) + 2 -> numteam + f: length: numteam - 1
+         */
+        int id = t2id.get(team).id;
+        int bestid = wins(team) + remaining(team);
+
+        FlowNetwork flow = new FlowNetwork(games.E() - games.degree(id) + numteam + 2); // one additional dummy vertex to make sure easy retrieval of team ID
+        int compID = 2;
+        int teamStart = games.E() - games.degree(id) + 2; // equal to team 0's id
+        StdOut.println("Team Start: " + teamStart);
+        
+        for(int i = 0; i < games.V(); i++) { // i shouldn't connected to id
+            if(i != id) {
+                for(Edge compete : games.adj(i)) {
+                    int i_against = compete.other(i);
+                    if(i_against != id) {
+                        // verified, neither i and i_against == id
+                        flow.addEdge(new FlowEdge(0, compID, compete.weight()));
+                        flow.addEdge(new FlowEdge(teamStart + i, 1, bestid - t2id.get(id2t.get(i)).wins));
+                        flow.addEdge(new FlowEdge(teamStart + i_against, 1, bestid - t2id.get(id2t.get(i_against)).wins));
+                        
+                        flow.addEdge(new FlowEdge(compID, teamStart + i, Double.POSITIVE_INFINITY));
+                        flow.addEdge(new FlowEdge(compID, teamStart + i_against, Double.POSITIVE_INFINITY));
+                        compID++;
+                    }
+                }
+            }
+        }
+        StdOut.println("compID: " + compID);        
+        return flow;
         
     }
     
@@ -80,8 +122,6 @@ public class BaseballElimination {
     
     public static void main(String[] args) {
         BaseballElimination test = new BaseballElimination(args[0]);   
-        StdOut.println(args[0]);
-
     }
     
 }
