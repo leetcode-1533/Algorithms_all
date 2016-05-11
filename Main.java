3ed57045a5891611b4763c2393f3344dc622bf20
Yingjie <1275963@gmail.com>
2016-05-11 16:08:15 +0800

    Started: Boggle Game

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StdOut.println("Hello Boggle");
+	// write your code here
+    }
+}
