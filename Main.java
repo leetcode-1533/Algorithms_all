3aefc0ccbeb51420bb8a1811a3072b46cc343d7d
Yingjie <1275963@gmail.com>
2016-05-11 17:03:33 +0800

    Sample code for "hashSET.contains()"

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 
 import edu.princeton.cs.algs4.StdOut;
-
+import java.util.HashSet;
 public class Main {
 
+
     public static void main(String[] args) {
-        StdOut.println("Hello Boggle");
+        HashSet<String> test = new HashSet<>();
+        String st1 = new String(new char[] {'a', 'b', 'c'});
+        String st2 = new String(new char[] {'a', 'b', 'c'});
+        StdOut.println(st1 == st2);
+        StdOut.println(st1.equals(st2));
+
+        test.add(st1);
+        StdOut.println(test.contains(st2));
 	// write your code here
     }
 }
