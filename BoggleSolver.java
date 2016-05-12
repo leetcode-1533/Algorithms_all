78cc46b31601f9e1e1218b211127edb617d48f74
Yingjie <1275963@gmail.com>
2016-05-12 11:25:43 +0800

    Fixing indexes

--- BoggleSolver.java
+++ BoggleSolver.java
@@ -5,13 +5,14 @@ import edu.princeton.cs.algs4.TrieSET;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.ArrayList;
-import java.lang.StringBuilder;
 
 
 public class BoggleSolver {
 
     private int size;
     private ArrayList<Bag<Integer>> dir;
+    private BoggleBoard boggleboard;
+
     private TrieSET trieset;
 
     private int center(int i, int j) {
@@ -41,25 +42,32 @@ public class BoggleSolver {
     }
 
     private void dir_init() {
-        int range = center(size, size);
-        dir = new ArrayList<>(range);
-        for (int index = 0; index < range; index++) {
+        int range = center(size - 1, size - 1);
+        dir = new ArrayList<>(range + 2);
+        Bag<Integer> master = new Bag<>();
+
+        for (int index = 0; index < range + 1; index++) {
             Bag<Integer> cur_dir = new Bag<>();
             int[] ij = index2ij(index);
             int i = ij[0];
             int j = ij[1];
 
-            if (i > 1)
+            if (i > 0)
                 cur_dir.add(up(i, j));
-            if (i < size)
+            if (i < size - 1)
                 cur_dir.add(down(i, j));
-            if (j > 1)
+            if (j > 0)
                 cur_dir.add(left(i, j));
-            if (j < size)
+            if (j < size - 1)
                 cur_dir.add(right(i, j));
 
             dir.add(cur_dir);
+
+            master.add(index); // master bag contain route to all bags
         }
+
+        // master bag
+        dir.add(master); // of index center(size, size) + 1
     }
 
     public BoggleSolver(String[] dictionary) {
@@ -69,15 +77,29 @@ public class BoggleSolver {
     }
 
     public Iterable<String> getAllValidWords(BoggleBoard board) {
+        boggleboard = board;
         size = board.rows();
-//        dir_init();
+        dir_init();
+
         Set<String> container = new HashSet<>();
+        dfs(new boolean[center(size - 1, size - 1) + 1], "", container, center(size - 1, size - 1) + 1);
 
         return container;
     }
 
-    private void dfs(boolean[] visited, StringBuilder str, Set<String> container) {
-
+    private void dfs(boolean[] visited, String str, Set<String> container, int index) {
+        for (Integer opts : dir.get(index)) {
+            if (!visited[opts]) {
+                visited[opts] = true;
+                int[] ij = index2ij(opts);
+                str += boggleboard.getLetter(ij[0], ij[1]);
+                if (trieset.longestPrefixOf(str) != null) {
+                    if (trieset.contains(str))
+                        container.add(str);
+                    dfs(visited.clone(), str, container, opts);
+                }
+            }
+        }
     }
 
     public static void main(String[] args)
@@ -87,7 +109,6 @@ public class BoggleSolver {
         BoggleSolver solver = new BoggleSolver(dictionary);
         BoggleBoard board = new BoggleBoard(args[1]);
         solver.getAllValidWords(board);
-        StdOut.println(solver.index2ij(15)[1]);
 //        int score = 0;
 //        for (String word : solver.getAllValidWords(board))
 //        {
