ed58f1a3e1df3fc985d57b451c13f9b14710472d
Yingjie <1275963@gmail.com>
2016-05-18 12:11:36 +0800

    Starting 'Circular Suffix Array'

---
+++ CircularSuffixArray.java
@@ -0,0 +1,19 @@
+public class CircularSuffixArray {
+    public CircularSuffixArray(String s) {
+        // circular suffix array of s
+    }
+
+    public int length() {
+        // length of s
+        return 0;
+    }
+
+    public int index(int i) {
+        // returns index of ith sorted suffix
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
