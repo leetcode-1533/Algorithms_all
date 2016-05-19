8d498b19dbc35b1312a9ffdad76eded765e4ea61
Yingjie <1275963@gmail.com>
2016-05-19 15:12:23 +0800

    BW decode Stage 1 completed

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -24,16 +24,33 @@ public class BurrowsWheeler {
                 break;
             }
         }
+
         BinaryStdOut.write(first);
         for (int i = 0; i < s.length(); i++) {
-//            StdOut.printf("%c ", charAt(s, s.length() - 1, CSA.index(i)));
             BinaryStdOut.write(charAt(s, s.length() - 1, CSA.index(i)));
         }
         BinaryStdOut.close();
     }
 
     public static void decode() {
+//        int first = BinaryStdIn.readInt();
+//        String text = BinaryStdIn.readString();
+        String text = "ARD!RCAAAABB";
+        int N = text.length();
+        int R = 256; // extended ASCII
 
+        // construct Next[]
+        int[] count = new int[R+1];
+        int[] next = new int[N];
+        for (int i = 0; i < N; i++)
+            count[text.charAt(i) + 1]++;
+        for (int r = 0; r < R; r++)
+            count[r+1] += count[r];
+        for (int i = 0; i < N; i++)
+            next[count[text.charAt(i)]++] = i;
+
+        for (int i = 0; i < N; i++)
+            StdOut.println(next[i]);
     }
 
     public static void main(String[] args) {
@@ -41,6 +58,7 @@ public class BurrowsWheeler {
             decode();
         else if (args[0].equals("-"))
             encode();
+        else throw new IllegalArgumentException("Illegal command line argument");
     }
 }
 
